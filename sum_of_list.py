b87ec1010f0440abb850d9f9b9ea89cc5e3e5338
Adam-Etyang <adametyang69>
2025-04-24 13:04:27 +0300

    sum commit

--- sum_of_list.py
+++ sum_of_list.py
@@ -1,11 +1,24 @@
-def sum_of_list(elements):
+import time
+
+
+# function that uses iteration to find the sum of elements in a list
+def sum_iterative(elements):
     num = 0
     for i in elements:
         num += i
     return num
 
 
-if "__main__" == __name__:
+# recursive version of the function above to find the sum of elements in a list
+def sum_recursive(elements):
+    if len(elements) == 0:
+        return 0
+    else:
+        return elements[0] + sum_recursive(elements[1:])
+
+
+# getting user input to add elements into a list and call the above funtions to find the sum and compare the speeds of the two different approaches
+if __name__ == "__main__":
     input_list = []
     while True:
         elements = input(
@@ -18,5 +31,23 @@ if "__main__" == __name__:
             input_list.append(numbers)
         except ValueError:
             print("please enter a valid number")
+    # timing iterative function:
+    start_iterative = time.time()
+    iterative_sum = sum_iterative(input_list)
+    end_interative = time.time()
+    iterative_time = end_interative - start_iterative
+
+    # timing recursive function
+    start_recursive = time.time()
+    recursive_sum = sum_recursive(input_list)
+    end_recursive = time.time()
+    recursive_time = end_recursive - start_recursive
+
+    # output of the results
     print(f"your list is :{input_list}\n")
-    print(f"the sum of elements in your list is:{sum(input_list)}")
+    print(
+        f"the sum of elements using iteration is :{iterative_sum} and it took {iterative_time:.6f}seconds"
+    )
+    print(
+        f"the sum of elements of the list using recursion is :{recursive_sum} and it took {recursive_time:.6f} seconds"
+    )
