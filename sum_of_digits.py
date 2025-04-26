04e2ef748eef34d9e5db36329575f68b4b9c8074
Adam-Etyang <adametyang69>
2025-04-26 16:32:50 +0300

    sum of digits

---
+++ sum_of_digits.py
@@ -0,0 +1,12 @@
+def sum_of_digits(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        total += digit
+        number = number // 10
+    return total
+
+
+if __name__ == "__main__":
+    number = int(input("enter a number: "))
+    print(f"the sum of the digits in the number{number} is {sum_of_digits(number)}")
